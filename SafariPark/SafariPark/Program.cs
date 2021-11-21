0deade11887427377c632d079ec8a0c5b3aeb516
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2021-11-21 16:11:45 +0300

    gitignore, stylecop, start project

---
+++ SafariPark/SafariPark/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SafariPark.Providers;
+using SafariPark.Providers.Abstractions;
+using SafariPark.Services;
+using SafariPark.Services.Abstractions;
+using SafariPark.UI;
+using SafariPark.UI.Abstractions;
+
+namespace SafariPark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
